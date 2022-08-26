c2d75dcc4cac203bacfc58aadd27d612c08ecaf0
hasanfuad <hasanmdfuad@hotmail.com>
2022-08-26 22:43:47 +0600

    commit handle multiple middleware and debugging redux application

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { applyMiddleware, createStore } from "redux";
 import { rootReducer } from "./rootReducer";
 
+import { composeWithDevTools } from "redux-devtools-extension";
 import myLogger from "./middlewares/myLogger";
 
 // redux logger
 import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger, logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(myLogger, logger))
+);
 
 export default store;
